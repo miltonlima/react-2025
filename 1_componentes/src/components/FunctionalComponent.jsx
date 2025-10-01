d65c9d28cfb625810f57b15fb384d40ac8dc869c
Milton Lima <milton.mlima@gmail.com>
2025-10-01 14:31:39 -0300

    first commit

---
+++ 1_componentes/src/components/FunctionalComponent.jsx
@@ -0,0 +1,18 @@
+// componente filho
+
+// criação da função
+function FunctionalComponent(){
+
+    // retorna um objeto com jsx
+    return (
+        <div>
+            <h2>Testando componente</h2>
+        </div>
+        
+    )
+
+
+}
+
+// ele exporta todos os dados
+export default FunctionalComponent;
