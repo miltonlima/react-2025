b8fac7d8522bb7fc8fb9a4528afdac72af34b6bc
Milton Lima <milton.mlima@gmail.com>
2025-10-01 14:59:30 -0300

    componente de classe

--- 1_componentes/src/App.jsx
+++ 1_componentes/src/App.jsx
@@ -2,7 +2,8 @@
 import './App.css'
 
 // Importar o componente o componente filho
-import FunctionalComponent from './components/FunctionalComponent'
+import FunctionalComponent from './components/FunctionalComponent';
+import ClassComponent from './components/ClassComponent';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <h1>React + Vite</h1>
       {/* utilizando o componente importado no JSX */}
       <FunctionalComponent />
+      <ClassComponent />
     </>
   )
 }
--- 1_componentes/src/components/ClassComponent.jsx
+++ 1_componentes/src/components/ClassComponent.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 class ClassComponent extends React.Component {
     
     render() {
